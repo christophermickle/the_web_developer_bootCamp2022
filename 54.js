96e9ed8c7dd9c1d42feb02b4832a6d1aff3391d0
Christopher Mickle <christopher@mickle.digital>
2022-04-05 18:54:49 -0700

    added code exercise 54 solution

---
+++ 54.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+
+document.querySelector('#egg').src='https://devsprouthosting.com/images/chicken.jpg';
+document.querySelector('#egg').alt='chicken';
+// document.querySelector('img')

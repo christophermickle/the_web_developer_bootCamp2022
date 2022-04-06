2ed81848b819e5f477d383d8379462b0ca37af62
Christopher Mickle <christopher@mickle.digital>
2022-04-06 16:02:23 -0700

    added notes for coding exercise 57

--- 57.js
+++ 57.js
@@ -1,4 +1,7 @@
 // WRITE YOUR CODE IN HERE:
+// Save all li elements to a variable 
+// iterate over the variable and toggle the highlight class
+
 const list = document.querySelectorAll('li');
 for (let li of list) {
     li.classList.toggle('highlight');

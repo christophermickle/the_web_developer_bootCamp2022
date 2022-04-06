e39df42c1f23ac2baad1349f9c2af12b8daba834
christophermickle <70554230+christophermickle@users.noreply.github.com>
2022-04-05 17:02:35 -0700

    Add files via upload

---
+++ 55.js
@@ -0,0 +1,8 @@
+
+// slect the id of container and apply text-alignment 
+// select and apply a variable to the image
+// apply styling to the image using the variable applied 
+document.querySelector('#container').style.textAlign='center';
+let img = document.querySelector('img'); 
+img.style.borderRadius='50%'
+img.style.width='150px'
